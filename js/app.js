8e823a6646acc0ac616224c6a9079c5ee112c192
alch3m1st <bryan.ale.3240@gmail.com>
2024-06-05 22:06:45 -0400

    the logic of the web is finish!!!! ignore the shity names of the variables, I need to clean some things in the code (next add styles to the container)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 const clicButton = document.querySelector('body');
-const hexLetters = ['A', 'B', 'C', 'D', 'E', 'F'].map(letter => letter.toLowerCase());
+// const hexLetters = ['A', 'B', 'C', 'D', 'E', 'F'].map(letter => letter.toLowerCase());
 
+function randomHexColor(longitud = 6) {
+    const hexLetters = 'abcdef0123456789'.toUpperCase();
+    let result = '#';
+    for (let i = 0; i < longitud; i++) {
+        result += hexLetters.charAt(Math.floor(Math.random() * hexLetters.length));
+      }
+      return result;
+}
+
+
+
+console.log(randomHexColor())
 
 function x() {
     clicButton.addEventListener('click', (event) => {
-        clicButton.style.backgroundColor = '#000'
+        clicButton.style.backgroundColor = randomHexColor();
+        document.getElementById('hexHead').textContent = randomHexColor()
     })
 }
 
